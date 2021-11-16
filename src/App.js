bd707fe7705f15241271322bf77d4964e3ccf1b9
Carlos Gomez <carlosandres0741@gmail.com>
2021-11-16 00:00:57 -0500

    Add: Page structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, {useState} from 'react'
+import './App.css'
 
 export default function App() {
-  const [option, setoption] = useState('')
+  const [option, setoption] = useState('success_kid')
+  const [text1, settext1] = useState('')
+  const [text2, settext2] = useState('')
 
   const onChangeOption = (event) => {
     setoption(event.target.value)
   }
 
+  const onChangeText1 = (event) => {
+    settext1(event.target.value)
+  }
+
+  const onChangeText2 = (event) => {
+    settext2(event.target.value)
+  }
+
   return (
     <>
-    <select id="option" onChange={onChangeOption}>
-      <option value="success_kid">Success Kid</option>
-      <option value="laughing_leo">Laughing Leo</option>
-      <option value="disaster_girl">Disaster Girl</option>
-      <option value="smart_person">Smart Person</option>
-    </select>    
-    <input type="text" placeholder="Texto 1"/>
-    <input type="text" placeholder="Texto 1"/>
-    <img src={`/img/${option}.png`}/>
+    <div>
+      <select id="option" onChange={onChangeOption}>
+        <option value="success_kid">Success Kid</option>
+        <option value="laughing_leo">Laughing Leo</option>
+        <option value="disaster_girl">Disaster Girl</option>
+        <option value="smart_person">Smart Person</option>
+      </select>    
+
+      <input type="text" placeholder="Texto 1" onChange={onChangeText1}/>
+      <input type="text" placeholder="Texto 2" onChange={onChangeText2}/>
+    </div>
+    <div id="meme">
+      <span>{text1}</span>
+      <img src={`/img/${option}.png`}/>
+      <span>{text2}</span>
+    </div>
     </>
   )
 }
